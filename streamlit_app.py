387aa46776843b9d7b25fe1ae91d25e9788ac7c4
rmorton8 <rmorton8@gmail.com>
2022-11-10 12:00:50 -0600

    Update streamlit_app.py
    updates

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,5 @@
 # Snowpark
 import snowflake.connector
-from snowflake.snowpark.session import Session
-from snowflake.snowpark.functions import avg, sum, col,lit
 import streamlit as st
 import pandas as pd
 
@@ -16,41 +14,7 @@ st.set_page_config(
      }
 )
 
-# Create Session object
-def create_session_object():
-    connection_parameters = {
-          "account": "ae08019.ca-central-1.aws",
-          "user": "mortonworkshop",
-          "password": "MtaXe0oO10eG",
-          "role": "accountadmin",
-          "warehouse": "compute_wh",
-          "database": "environment_data_atlas",
-          "schema": "environment"
-       }
-    session = Session.builder.configs(connection_parameters).create()
-    print(session.sql('select current_warehouse(), current_database(), current_schema()').collect())
-    return session
-
 if __name__ == "__main__":
-     session = create_session_object()
-
-     # Create Snowpark DataFrames that loads data from Knoema: Environmental Data Atlas
-     snow_df_co2 = session.table("ENVIRONMENT.EDGARED2019").filter(col('Indicator Name') == 'Fossil CO2 Emissions').filter(col('Type Name') == 'All Type')
-     snow_df_co2 = snow_df_co2.group_by('Location Name').agg(sum('$16').alias("Total CO2 Emissions")).filter(col('Location Name') != 'World').sort('Location Name')
-
-     # Forest Occupied Land Area by Country
-     snow_df_land = session.table("ENVIRONMENT.\"WBWDI2019Jan\"").filter(col('Series Name') == 'Forest area (% of land area)')
-     snow_df_land = snow_df_land.group_by('Country Name').agg(sum('$61').alias("Total Share of Forest Land")).sort('Country Name')
-
-     # Total Municipal Waste by Country
-     snow_df_waste = session.table("ENVIRONMENT.UNENVDB2018").filter(col('Variable Name') == 'Municipal waste collected')
-     snow_df_waste = snow_df_waste.group_by('Location Name').agg(sum('$12').alias("Total Municipal Waste")).sort('Location Name')
-
-     # Convert Snowpark DataFrames to Pandas DataFrames for Streamlit
-     pd_df_co2  = snow_df_co2.to_pandas()
-     pd_df_land = snow_df_land.to_pandas() 
-     pd_df_waste = snow_df_waste.to_pandas().iloc[:11]
-
      # Add header and a subheader
      st.title('Streamlit Demo -- this is a streamlit title function')
      st.subheader("Powered by Snowpark for Python and Snowflake Data Marketplace | Made with Streamlit -- subheader function")
@@ -81,13 +45,4 @@ if __name__ == "__main__":
                                                                "bank misses detecting fraud and no hold is placed"))
 
 
-         # Display an interactive chart to visualize CO2 Emissions by Top N Countries
-     with st.container():
-         st.subheader('CO2 Emissions by Top N Countries')
-         with st.expander(""):
-             emissions_threshold = st.slider(label='Emissions Threshold',min_value=5000, value=20000, step=5000)
-             pd_df_co2_top_n = snow_df_co2.filter(col('Total CO2 Emissions') > emissions_threshold).to_pandas()
-             st.bar_chart(data=pd_df_co2_top_n.set_index('Location Name'), width=850, height=500, use_container_width=True)
-     #picture = st.camera_input('Take a picture')
-     #if picture:
-     #    st.image(picture)
+         
