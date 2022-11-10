d8de0753086b8ffc8cfac2faa0976a1aa235ee93
rmorton8 <rmorton8@gmail.com>
2022-11-10 14:32:06 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import snowflake.connector
 
 import streamlit as st
 import pandas as pd
-import plotly.express as px
+# import plotly.express as px
 
 st.set_page_config(
     page_title="Streamlit Demo",
