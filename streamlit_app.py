9f97b10c21be68c806317ce486394f79533c4392
rmorton8 <rmorton8@gmail.com>
2022-11-10 19:46:00 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,7 +47,7 @@ def grab_data_from_snowflake(table_name):
     my_cnx.close()
     return output
 
-@st.cache
+@st.cache(allow_output_mutation=True)
 def grab_and_plot_data(table_name, values):
     votes = grab_data_from_snowflake(table_name)
     if len(votes) >= 2:
